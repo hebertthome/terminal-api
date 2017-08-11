2fd797965ddd3a62126823f0e19d8d8c2f0d051b
hebert <hebert.santos@HebertSantos.local>
2017-08-11 12:30:12 -0300

    Setting  swagger

--- src/main/java/br/com/muxi/exame/config/SwaggerConfig.java
+++ src/main/java/br/com/muxi/exame/config/SwaggerConfig.java
@@ -13,24 +13,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-        		.useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.muxi.exame.resources.exposed"))
-                .build()
-                .apiInfo(metaData());
-    }
-    private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo(
-                "Api Documentation",
-                "Api Documentation",
-                "1.0",
-                "Terms of service",
-                new Contact("Hebert Thomé", "https://github.com/hebertthome/terminal-api", "hebert.t.santos@gmail.com"),
-               "Apache 2.0",
-                "https://www.apache.org/licenses/LICENSE-2.0");
-        return apiInfo;
-    }
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.select()
+				.apis(RequestHandlerSelectors
+						.basePackage("br.com.muxi.exame.resources.documenteded"))
+				.build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		ApiInfo apiInfo = new ApiInfo("Api Documentation", "Api Documentation",
+				"1.0", "Terms of service", new Contact("Hebert Thomé",
+						"https://github.com/hebertthome/terminal-api",
+						"hebert.t.santos@gmail.com"), "Apache 2.0",
+				"https://www.apache.org/licenses/LICENSE-2.0");
+		return apiInfo;
+	}
 }
